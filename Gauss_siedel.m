c43fde2ea7f5da4e8f7605d7c6e3381439639f24
00KL <lucasmoraesplay@gmail.com>
2019-11-14 13:50:25 -0300

    Add files via upload

--- Gauss_siedel.m
+++ Gauss_siedel.m
@@ -1,4 +1,4 @@
-function [x]=Gauss_siedel(N,v1,v2,v3,b,tol,T0,TL)  
+function [x]=Gauss_siedel(N,v1,v2,v3,b,tol)  
       %N = dimensão
       %v1 = diagonal principal
       %v2 = subdiagonal superior
@@ -13,7 +13,7 @@ function [x]=Gauss_siedel(N,v1,v2,v3,b,tol,T0,TL)
       
       while(tol <= erro )
             x0 = x;
-            x(1) = b(1) - v2(1)*x(2) - T0;
+            x(1) = b(1) - v2(1)*x(2);
             x(1) = x(1)/v1(1); % A primeira iteração não segue o padrão por consultar
                                % apenas o primeiro valor de v1(diagnoal principal)
                                % e o primeiro valor de v2(subdiagonal superior)
@@ -24,7 +24,7 @@ function [x]=Gauss_siedel(N,v1,v2,v3,b,tol,T0,TL)
                                     % e o último valor de v2(subdiagonal superior)
             endfor
             
-            x(N) = b(N) - v3(N-1)*x(N-1) - TL;
+            x(N) = b(N) - v3(N-1)*x(N-1);
             x(N) = x(N)/v1(N);
             
             erro = norm(x - x0, inf) / norm(x, inf); %norm -> ||x-x0||
--- main.m
+++ main.m
@@ -1,23 +1,23 @@
 %N = 3;
-%v1 = [8,7,6];
-%v2 = [1,1,0];
-%v3 = [2,2,0];
+%a = [8,7,6];
+%b = [2,2,0];
+%c = [1,1,0];
 %[8 1 0]
 %[2 7 1]
 %[0 2 6]
 
 
-%b = [3,5,7];
+%F = [3,5,7];
 %tol = 0.001;
-%Gauss_siedel(N,v1,v2,v3,b,tol)
+%Gauss_siedel(N,a,c,b,F,tol)
 %A = [8,1,0;
 %     2,7,1;
 %     0,2,6];
-%b = [3;5;7];
+%F = [3;5;7];
 %x = [0;0;0];
-%gauss_seidel(A,b,x,tol,N)
+%gauss_seidel(A,F,x,tol,N)
 
-disp ("Digite uma opção:")
+disp ("Digite uma opcao:")
 disp ("1 - Resolver um sistema Ax = b (com matriz tridiagonal) de dimens~ao n.")
 disp ("2 - Resolver o PVC para o problema A.")
 disp ("3 - Resolver o PVC para o problema B.")
@@ -25,16 +25,21 @@ disp ("4 - Resolver o PVC para o problema C.")
 disp ("5- Sair")
 escolha = input("Escolha:");
 
+      %A escolha do nome F ao inves de b 
+      %e por conta do padrÃ£o de se usar b
+      %como a subdiagonal inferior 
+      %F = zeros(m-1,1);
+
 switch (escolha)
     
     case 1
-      N = input("Dimensão da matriz: ");
-      v1 = input("Vetor Diagonal Principal: ");
-      v2 = input("Vetor subdiagonal superior: ");
-      v3 = input("Vetor subdiagonal inferior: ");
-      b = input("Vetor b: ");
+      N = input("Dimensao da matriz: ");
+      a = input("Vetor Diagonal Principal: ");
+      b = input("Vetor subdiagonal inferior: ");
+      c = input("Vetor subdiagonal superior: ");
+      F = input("Vetor b: ");
       tol = 10^-10;
-      Gauss_siedel(N,v1,v2,v3,b,tol)
+      Gauss_siedel(N,a,c,b,F,tol)
     
     case 2
       disp("Problema A")
@@ -42,103 +47,78 @@ switch (escolha)
       m = 30;
       T0 = 20;
       TL = 100;
-      tol = 10^-10;
+      tol = 10^(-10);
       
-      %como f(x) = 0 não é necessário
-      %calcular h, pois h iria somente 
-      %multiplicar os valores f(xi)
+      %Calculando o h
+      %DONE concertar h nos outros calculos
+      h = 3/(m-1);
+          
+      %menos o primeiro e o ultimo
+      x = [0:h:3,1]; % x tem m+1 elementos
+      x = transpose(x)
+      %Como a funcao a f(x) = 0 
+      %a todos os valore serao nulos
+      F = zeros(m-1,1);
+      F(1) = -T0;   %em toda iteraÃ§Ã£o da primeira linha sera subtraido T0
+      F(m-1) = -TL; %em toda iteraÃ§Ã£o da Ãºltima linha sera subtraido TL
       
-      %Como a função é f(x) = 0 
-      %a todos os valore serão nulos
-      %menos o primeiro e o último
-      b = zeros(m,1);
-      b(1) = -T0;
-      b(m+1) = -TL;
+      %Matriz da discretizacao
+      a = ones(m - 1,1);
+      a = a*(-2);
+      b = ones(m - 1,1);
+      c = ones(m - 1,1);
       
-      %Matriz da discretização
-      v1 = ones(m - 1,1);
-      v1 = v1*(-2);
-      v2 = ones(m - 1,1);
-      v3 = ones(m - 1,1);
-      
-      Gauss_siedel (m-1, v1, v2, v3, b, tol,0,0)
+      %DONE tirar t0 e tl do for
+      T = Gauss_siedel (m-1, a, b, c, F, tol);
+      T;
+      %DONE acrescentar valores de T0 e TL
+      T = [T0;T;TL];
+      T
+      %DONE plotar grafico plot(x,T)
+      plot(x, "x" , T, "T")
       
     case 3
       disp("Problema B")
-      %Dados do problema A
-      m = 30;
-      T0 = 20;
+      %Dados do problema B
+      m = 300;
+      T0 = 0;
       TL = 100;
       tol = 10^-10;
       
-      %como f(x) = 0 não é necessário
-      %calcular h, pois h iria somente 
-      %multiplicar os valores f(xi)
+      %Calculando o h
+      %DONE concertar h nos outros calculos
+      h = 3/(m-1);
       
-      %Como a função é f(x) = 0 
-      %a todos os valore serão nulos
-      %menos o primeiro e o último
-      b = zeros(m-1,1);
+      %DONE mudar valor de x
+      x = 0:h:3; % x tem m+1 elementos
+      %DONE concertar h nos outros fors
+      F = zeros(m-1,1);
+      for i = 1:(m-1)
+        F(i) = (h^2)*10*sin(x(i+1)); % x sÃ³ percorrer os nÃ³s internos
+      endfor
+      F(1) = F(1) - T0;    %em toda iteraÃ§Ã£o da primeira linha sera subtraido T0
+      F(m-1) = F(m-1) - TL;%em toda iteraÃ§Ã£o da Ãºltima linha sera subtraido TL
       
-      %Matriz da discretização
-      v1 = ones(m - 1,1);
-      v1 = v1*(-2);
-      v2 = ones(m - 1,1);
-      v3 = ones(m - 1,1);
+      %Matriz da discretizacao
+      a = ones(m - 1,1);
+      a = a*(-2);
+      b = ones(m - 1,1);
+      c = ones(m - 1,1);
       
-      Gauss_siedel (m-1, v1, v2, v3, b, tol,T0,TL)
+      %DONE tirar t0 e tl do for
+      T = Gauss_siedel (m-1, a, b, c, F, tol);
+      
+      %DONE acrescentar valores de T0 e TL
+      T = [T0;T;TL];
+    
+      %DONE plotar grafico plot(x,T)
+      plot(x, "-;X;", T , "-;T;")
+
    
    case 4
       disp("Problema C")
-      %Dados do problema A
-      m = 300;
-      T0 = 20;
-      TL = 100;
-      tol = 10^-10;
-      
-      %Calculando o h
-      h = (TL - T0)/(m-1);
-      
-      %Como a função é f(x) = 0 
-      %a todos os valore serão nulos
-      %menos o primeiro e o último
-      b = zeros(m-1,1);
-      for i = 1:(m-1)
-        b(i) = (h^2)*10*sin(i);
-      endfor
-      
-      %Matriz da discretização
-      v1 = ones(m - 1,1);
-      v1 = v1*(-2);
-      v2 = ones(m - 1,1);
-      v3 = ones(m - 1,1);
-      
-      Gauss_siedel (m-1, v1, v2, v3, b, tol,T0,TL)
+   
     case 5
       disp(" Progrma finalizado ")
-      disp("Problema C")
-      %Dados do problema A
-      m = 25;
-      T0 = 2;
-      TL = 10;
-      tol = 10^-10;
       
-      %Calculando o h
-      h = (TL - T0)/(m-1);
-      
-      %Como a função é f(x) = 0 
-      %a todos os valore serão nulos
-      %menos o primeiro e o último
-      b = zeros(m-1,1);
-      for i = 1:(m-1)
-        b(i) = (h^2)*(i^2);
-      endfor
-      
-      %Matriz da discretização
-      v1 = ones(m - 1,1);
-      v1 = v1*(-2);
-      v2 = ones(m - 1,1);
-      v3 = ones(m - 1,1);
-      
-      Gauss_siedel (m-1, v1, v2, v3, b, tol,T0,TL)
   endswitch
